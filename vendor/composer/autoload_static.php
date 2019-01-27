a47aff9e3b80853ec2d78992678eec4d9e1df36c
cleomar <cpspeartree@gmail.com>
2019-01-27 11:41:09 -0200

    upando a classe autloadgit add --all

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,8 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitda1adf281f066e4b6006731879c95b4e
 {
-    public static $fallbackDirsPsr4 = array (
-        0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+    public static $prefixLengthsPsr4 = array (
+        'H' => 
+        array (
+            'Hcode\\' => 6,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Hcode\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/hcodebr/php-classes/src',
+        ),
     );
 
     public static $prefixesPsr0 = array (
@@ -41,7 +51,8 @@ class ComposerStaticInitda1adf281f066e4b6006731879c95b4e
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->fallbackDirsPsr4 = ComposerStaticInitda1adf281f066e4b6006731879c95b4e::$fallbackDirsPsr4;
+            $loader->prefixLengthsPsr4 = ComposerStaticInitda1adf281f066e4b6006731879c95b4e::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInitda1adf281f066e4b6006731879c95b4e::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInitda1adf281f066e4b6006731879c95b4e::$prefixesPsr0;
             $loader->classMap = ComposerStaticInitda1adf281f066e4b6006731879c95b4e::$classMap;
 
